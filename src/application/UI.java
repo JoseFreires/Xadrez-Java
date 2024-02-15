7657b1b9644935017fdefbf08af8fd25fde88e35
Jose Freires <joseguifreires@gmail.com>
2024-02-14 21:29:56 -0300

    Created the Check Logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -59,6 +59,10 @@ public class UI {
 		System.out.println();
 		System.out.println("Turn: " + chessmatch.getTurn());
 		System.out.println("Waiting: " + chessmatch.getCurrentPlayer());
+		
+		if(chessmatch.getCheck()) {
+			System.out.println("CHECK!");
+		}
 	}
 
 	public static void printBoard(ChessPiece[][] pieces) {
